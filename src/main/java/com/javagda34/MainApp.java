b616b953075e8651bc09f4bf2ee511c51461eed3
teksah <teksah@gmail.com>
2020-08-09 10:44:22 +0200

    third commit

--- src/main/java/com/javagda34/MainApp.java
+++ src/main/java/com/javagda34/MainApp.java
@@ -11,9 +11,7 @@ public class MainApp {
     public static void main(String[] args) {
 
         final ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
-
         final AccountDAOImpl accountDAO = context.getBean(AccountDAOImpl.class);
-
 
         Arrays.stream(context.getBeanDefinitionNames())
                 .forEach(System.out::println);
--- src/main/java/com/javagda34/service/TransferService.java
+++ src/main/java/com/javagda34/service/TransferService.java
@@ -1,6 +1,5 @@
 package com.javagda34.service;
 
-import org.springframework.stereotype.Service;
-
 public interface TransferService {
+
 }
